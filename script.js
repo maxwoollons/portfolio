932bc8909d170aa699f3d5606f776b26a77c002f
Max Woollons <50788222+maxwoollons@users.noreply.github.com>
2022-04-02 22:07:51 +1000

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 let toggle = document.getElementById("cb");
 let navbar = document.getElementById("navbar");
+let parallaxtext = document.getElementById("parallax");
+let css = document.getElementById("css");
+let html = document.getElementById("html");
+let form = document.getElementById("form");
+
 
 if (localStorage.getItem("tutorial") === "true") {
     document.getElementsByClassName("tutorial")[0].style.display = "none";
@@ -19,6 +24,7 @@ if (localStorage.getItem("darkmode") === "true" ? toggle.checked = true : toggle
     document.body.style.backgroundColor = "#242526";
     document.body.style.color = "white";
     navbar.style.backgroundColor = "#424040";
+    form.style.backgroundColor = "#474747";
 }
 
 
@@ -31,7 +37,11 @@ let darkmode = () => {
         document.body.style.color = "white";
         navbar.style.backgroundColor = "#424040";
         localStorage.setItem("tutorial", "true");
+        parallaxtext.style.color = "white";
+        form.style.backgroundColor = "#474747";
+      
         hidetut()
+
     } else {
         localStorage.setItem("darkmode", "false");
         // is lightmode
@@ -39,6 +49,9 @@ let darkmode = () => {
         document.body.style.color = "black";
         navbar.style.backgroundColor = "#666565";
         localStorage.setItem("tutorial", "true");
+        parallaxtext.style.color = "white";
+        form.style.backgroundColor = "#e0dcdc";
+
         hidetut()
     }
 }
